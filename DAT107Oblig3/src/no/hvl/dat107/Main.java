5265bececac7f522af23714043d678d5dce4b9fa
h591099 <89256511+591099T@users.noreply.github.com>
2022-03-25 12:05:01 +0100

    kode

--- DAT107Oblig3/src/no/hvl/dat107/Main.java
+++ DAT107Oblig3/src/no/hvl/dat107/Main.java
@@ -1,9 +1,17 @@
 package no.hvl.dat107;
 
+import no.hvl.dat107.dao.AnsattDAO;
+import no.hvl.dat107.dao.ProsjektDAO;
+import no.hvl.dat107.entity.Ansatt;
+import no.hvl.dat107.entity.Prosjekt;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AnsattDAO ansattDAO = new AnsattDAO();
+        ProsjektDAO prosjektDAO = new ProsjektDAO();
+        
+        
 
 	}
 
